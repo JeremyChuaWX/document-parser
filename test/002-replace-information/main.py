80753080c074a5e6e9749aa6c4c0407d858c17e6
Jeremy Chua <chua.jeremy.wx@gmail.com>
2024-09-21 17:41:52 +0800

    feat: prompt-v1

--- test/002-replace-information/main.py
+++ test/002-replace-information/main.py
@@ -11,7 +11,9 @@ OLLAMA_ADDRESS = os.environ["OLLAMA_ADDRESS"]
 
 timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
 document_path = os.path.join(ARTIFACTS_PATH, FILENAME)
-output_path = os.path.join(OUTPUTS_PATH, f"{FILENAME}_{timestamp}_output.json")
+output_path = os.path.join(
+    OUTPUTS_PATH, f"{FILENAME.split(".")[0]}_{timestamp}_output.json"
+)
 client = Client(host=OLLAMA_ADDRESS)
 
 
@@ -32,19 +34,27 @@ def query(text, html):
     ```
 
     The text in the HTML is inaccurate.
-    Using the raw text as the source of truth, replace the text inside the HTML table elements with the respective snippets.
+    Using the raw text as the source of truth, replace the text inside `<td></td>` tags with the respective snippets from the raw text.
+    Do not change the table structure. Do not modify any HTML tags. Only replace the text between `<td></td>` tags.
+    Return the final HTML after performing the replacements. Do not say any thing else before or after.
+    I will KILL MYSELF if you don't only return HTML.
 
-    Return ONLY the modified HTML. I will KILL MYSELF if you don't only return HTML.
+    Example:
+
+    ```input
+    <table><tbody><td></td><td></td><td>"inaccurate text from HTML"</td>...</tbody></table>
+    ```
+
+    ```output
+    <table><tbody><td></td><td></td><td>"accurate text from raw text"</td>...</tbody></table>
+    ```
     """
-    return client.chat(
-        model="llama3.1",
-        messages=[
-            {
-                "role": "user",
-                "content": prompt,
-            }
-        ],
-    )["message"]["content"]
+    response = client.generate(
+        model="hermes3:8b",
+        prompt=prompt,
+    )["response"]
+    print(html, "\n\n", response, "\n\n")
+    return response
 
 
 tables = None
@@ -52,9 +62,11 @@ tables = None
 with open(document_path, "r") as f:
     tables = json.loads(f.read())["tables"]
 
-responses = {
-    "responses": [query(table["text"], table["html"]) for table in tables],
-}
+# responses = {
+#     "responses": [query(table["text"], table["html"]) for table in tables],
+# }
+
+responses = {"responses": query(tables[0]["text"], tables[0]["html"])}
 
 with open(output_path, "w") as f:
     f.write(json.dumps(responses))
